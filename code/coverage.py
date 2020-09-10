27caf52c0d1f3216cf17f47d80183e513a9d818b
Ibrahim <ibrahimemohamed33@gmail.com>
2020-09-09 23:55:49 -0400

    removed the export to csv lines

--- code/coverage.py
+++ code/coverage.py
@@ -8,5 +8,3 @@ def join_file(__file__): # Make sure file is a string
 df = pd.read_csv(join_file('ALL_SPLITS-gene_non_outlier_coverages.txt'), sep = '\t').set_index('gene_callers_id')
 df_sigma = pd.read_csv(join_file('ALL_SPLITS-gene_non_outlier_coverage_stds.txt'), sep = '\t').set_index('gene_callers_id')
 
-df.to_csv('Coverage.csv')
-df_sigma.to_csv('Coverage_Sigma.csv')
